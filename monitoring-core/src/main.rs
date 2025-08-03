1516f5b04ad517ea77f1069df7abb244f10b90b6
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2025-08-04 01:15:47 +0200

    connect rust client to AWS

--- monitoring-core/src/main.rs
+++ monitoring-core/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    monitoring_core::run();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv()?;
+    monitoring_core::run().await?;
+    Ok(())
 }
