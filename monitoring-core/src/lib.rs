185fa8f3914158bee12df85d52e7e621a48fd34a
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2025-08-08 10:44:09 +0200

    push humidity data

--- monitoring-core/src/lib.rs
+++ monitoring-core/src/lib.rs
@@ -6,62 +6,51 @@ use aws_iot_device_sdk_rust::{
 use chrono::Utc;
 use prost::Message;
 use rand::Rng;
-use rumqttc::{ClientError, ConnectionError, Packet, QoS};
+use rumqttc::{ClientError, Packet, QoS};
 use std::time::Duration;
 
-use crate::types::Temperature;
+use crate::types::{Humidity, Temperature};
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let (temp_client, temp_eventloop_stuff) =
+    let (temp_client, temp_eventloop) =
         AWSIoTAsyncClient::new(aws_settings("rust-client-1")?).await?;
-    println!("Connected to AWS IoT Core");
-    // let (humidity_client, humidity_eventloop_stuff) = AWSIoTAsyncClient::new(aws_settings("rust-client-2")?).await?;
+    let (humidity_client, humidity_eventloop) =
+        AWSIoTAsyncClient::new(aws_settings("rust-client-2")?).await?;
 
-    // tokio::spawn(async move {
-    //     let humidity_device_id = "humidity_sensor_1".to_string();
-    //     for humidity in random_number_generator().take(10) {
-    //         client
-    //             .publish(
-    //                 "monitoring/humidity",
-    //                 QoS::AtLeastOnce,
-    //                 Humidity {
-    //                     device_id: humidity_device_id.clone(),
-    //                     value: humidity as f32,
-    //                     timestamp: Utc::now().timestamp_millis() as u64,
-    //                 }
-    //                 .encode_to_vec(),
-    //             )
-    //             .await
-    //             .inspect_err(|e| eprintln!("Failed to publish humidity: {}", e))
-    //             .unwrap();
-    //         tokio::time::sleep(Duration::from_secs(1)).await;
-    //     }
-    // });
+    let mut temp_receiver = temp_client.get_receiver().await;
+    let mut humidity_receiver = humidity_client.get_receiver().await;
 
-    let mut receiver1 = temp_client.get_receiver().await;
-
-    let recv1_thread = tokio::spawn(async move {
+    let recv_thread = tokio::spawn(async move {
         loop {
-            if let Ok(event) = receiver1.recv().await {
-                println!("Received event on receiver1: {:?}", event);
+            if let Ok(event) = temp_receiver.recv().await {
                 match event {
                     Packet::Publish(p) => {
                         println!("Received message {:?} on topic: {}", p.payload, p.topic)
                     }
-                    _ => println!("Got event on receiver1: {:?}", event),
+                    _ => println!("Got event on temp_receiver: {:?}", event),
+                }
+            }
+            if let Ok(event) = humidity_receiver.recv().await {
+                match event {
+                    Packet::Publish(p) => {
+                        println!("Received message {:?} on topic: {}", p.payload, p.topic)
+                    }
+                    _ => println!("Got event on humidity_receiver: {:?}", event),
                 }
             }
         }
     });
 
     let listen_thread = tokio::spawn(async move {
-        async_event_loop_listener(temp_eventloop_stuff).await?;
-        Ok::<(), ConnectionError>(())
+        tokio::join!(
+            async_event_loop_listener(temp_eventloop),
+            async_event_loop_listener(humidity_eventloop)
+        )
     });
 
-    let publisher = tokio::spawn(async move {
+    let temp_publisher = tokio::spawn(async move {
         let temperature_device_id = "temperature_sensor_1".to_string();
-        for temp in random_number_generator().take(10) {
+        for temp in random_number_generator() {
             temp_client
                 .publish(
                     "monitoring/temperature",
@@ -79,8 +68,33 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
         Ok::<(), ClientError>(())
     });
 
-    match tokio::join!(publisher, recv1_thread, listen_thread) {
-        (Ok(_), Ok(_), Ok(_)) => (),
+    let humidity_publisher = tokio::spawn(async move {
+        let humidity_device_id = "humidity_sensor_1".to_string();
+        for humidity in random_number_generator() {
+            humidity_client
+                .publish(
+                    "monitoring/humidity",
+                    QoS::AtLeastOnce,
+                    Humidity {
+                        device_id: humidity_device_id.clone(),
+                        value: humidity as f32,
+                        timestamp: Utc::now().timestamp_millis() as u64,
+                    }
+                    .encode_to_vec(),
+                )
+                .await?;
+            tokio::time::sleep(Duration::from_secs(1)).await
+        }
+        Ok::<(), ClientError>(())
+    });
+
+    match tokio::join!(
+        temp_publisher,
+        humidity_publisher,
+        recv_thread,
+        listen_thread
+    ) {
+        (Ok(_), Ok(_), Ok(_), Ok(_)) => (),
         _ => panic!("Error in threads"),
     }
     Ok(())
